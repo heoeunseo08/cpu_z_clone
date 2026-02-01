8b42d93b12a420aa5a7474d1d68b5f49ab4bdc9a
heoeunseo <heoeunseong140@gmail.com>
2026-02-01 15:13:45 +0900

    appbar 세팅중

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,3 +1,4 @@
+import 'package:cpu_z_clone/utils/utils.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,74 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('cpu_z')));
+    return DefaultTabController(
+      length: 7,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Image.asset('assets/logo.png', height: 45, width: 45),
+                  SizedBox(width: 6),
+                  Text(
+                    "CPU-Z",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Image.asset('assets/trophy.png', width: 29, height: 29),
+                  SizedBox(width: 10),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.more_vert,
+                      size: 24,
+                      color: Color(0xffA6A6A6),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          bottom: TabBar.secondary(
+            isScrollable: true,
+            labelStyle: TextStyle(
+                color: Color(0xffAE8AEA),
+                fontSize: 20,
+                fontWeight: FontWeight.w500
+            ),
+            unselectedLabelStyle:TextStyle(
+                color: Color(0xffA6A6A6),
+                fontSize: 20,
+                fontWeight: FontWeight.w500
+            ),
+            indicatorColor: Color(0xff3303AD),
+            indicatorSize: TabBarIndicatorSize.tab,
+            tabs: <Widget>[
+              Tab(child: Text("SOC")),
+              Tab(child: Text("DEVICE")),
+              Tab(child: Text("SYSTEM")),
+              Tab(child: Text("BATTERY")),
+              Tab(child: Text("THERMAL")),
+              Tab(child: Text("SENSORS")),
+              Tab(child: Text("ABOUT")),
+            ],
+          ),
+          backgroundColor: AppColor.background,
+          automaticallyImplyLeading: false,
+        ),
+        body: Center(
+          child: Text('cpu_z'),
+        ),
+      ),
+    );
   }
 }
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-class Utils {
+class AppColor {
   static final primary = Color(0xffAE8AEA);
   static final background = Color(0xff121212);
+}
 
-
+class Utils {
   static ThemeData appTheme() {
     return ThemeData(
       colorScheme: .fromSeed(seedColor: Colors.black),
-      primaryColor: primary,
-      scaffoldBackgroundColor: background
+      primaryColor: AppColor.primary,
+      scaffoldBackgroundColor: AppColor.background,
     );
   }
 }
